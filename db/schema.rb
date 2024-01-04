041f114d8959fdf5bf1f3a13ddb4ff1193842bc5
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-04 21:04:06 +0800

    Added model for portfolio, modified corresponding spec file to pass test.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_16_014114) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_121326) do
+  create_table "portfolios", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_portfolios_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_16_014114) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "portfolios", "users"
 end
--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Portfolio, type: :model do
-    it 'belongs to a user' do
-        expect(subject).to belong_to(:user)
-    end
+ # pending "add some examples to (or delete) #{__FILE__}"
+ it 'belongs to a user' do
+    user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john.doe@sample.com',
+        password: 'password',
+        role: 0,
+        status: 0)
 
-    it 'validates presence of stock' do
-        expect(subject).to validate_presence_of(:stock)
-    end
+    portfolio = Portfolio.new(user: user)
+
+    user.save!
+    portfolio.save!
+    portfolio.reload
+
+    expect(portfolio.user).to eq(user)
+  end
 end
