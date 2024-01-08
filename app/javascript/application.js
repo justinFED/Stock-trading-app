ac28b85936401e176ceebb6bedd1c92bce884ec9
Justin Cantillo <justin.tech96@gmail.com>
2024-01-08 13:46:14 +0800

    Admin Dashboard

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,59 @@ import "controllers"
 //= require jquery3
 //= require popper
 //= require bootstrap
+
+
+document.addEventListener("turbo:load", () => {
+  
+  const generateRandomData = () => {
+    const data = [];
+    for (let i = 0; i < 50; i++) {
+      data.push(Math.random() * 40 + 60); 
+    }
+    return data;
+  };
+
+  const stockData = {
+    labels: Array.from({ length: 50 }, (_, i) => i.toString()), 
+    datasets: [{
+      label: "Fluctuating Pattern",
+      data: generateRandomData(),
+      fill: false,
+      borderColor: "#21ce99", 
+      borderWidth: 2,
+      lineTension: 0.1, 
+      pointRadius: 0, 
+    }]
+  };
+
+  const ctx = document.getElementById("stockChart").getContext("2d");
+
+  const stockChart = new Chart(ctx, {
+    type: "line",
+    data: stockData,
+    options: {
+      scales: {
+        x: [{
+          type: 'linear',
+          position: 'bottom'
+        }],
+        y: [{
+          ticks: {
+            beginAtZero: true,
+            max: 100, 
+          }
+        }]
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        duration: 0
+      },
+    }
+  });
+});
+
+
+
+
+
