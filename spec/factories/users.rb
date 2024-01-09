deccf303318d558751a9f80e435972043f99db34
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-09 17:29:21 +0800

    Added gem rails-controller-testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user do
         first_name { 'John' }
         last_name { 'Doe' }
-        email { 'john@doe.com' }
+        sequence(:email) { |n| "user#{n}@example.com" }
         password { 'password' }
         role { 'trader' }
         status { 'pending' }
