eedb3a93e0cce0855207a22e3b3e26c73df662ec
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-10 17:04:07 +0800

    Added initial test for trader dashboard

--- spec/requests/trader/dashboard_spec.rb
+++ spec/requests/trader/dashboard_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Trader::Dashboards", type: :request do
-  describe "GET /index" do
-    # pending "add some examples (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+
+  
+  before do
+    @approved_trader = create(:approved_trader)
+    sign_in @approved_trader
   end
+    # pending "add some examples (or delete) #{__FILE__}"
+
+  describe 'GET #index' do
+    it 'renders the index template' do
+      get trader_dashboard_index_path
+      expect(response).to render_template :index
+    end
+  end
+
 end
