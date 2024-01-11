94412f620307afa037dcd3da1bc2d7415fef1a8d
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-11 19:46:27 +0800

    Removed conflicts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-<<<<<<< HEAD
-  
-    def after_sign_in_path_for(resource)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
       if resource.admin?
-        admin_dashboard_index_path
+          admin_dashboard_index_path
       else
-        trader_dashboard_index_path
-      end
-=======
-
-    def after_sign_in_path_for(resource)
-        if resource.admin?
-            admin_dashboard_index_path
-        else
-            trader_dashboard_index_path
-         end
-    end
-
-    protected
-
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
->>>>>>> 09588c33104fc579383e97f7710d31d199268ea1
-    end
-
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
-    end
+          trader_dashboard_index_path
+       end
   end
-  
+
+  protected
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
+  end
+end
