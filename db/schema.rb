462b42c193bb3b7c5105ac2a2a5fcb9752105e27
Justin Cantillo <justin.tech96@gmail.com>
2024-01-12 00:11:18 +0800

    Switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_04_121326) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "portfolios", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_portfolios_on_user_id"
