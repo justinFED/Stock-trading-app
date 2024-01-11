946019b1f1d442ff8c0b36714be518ca572f8854
Justin Cantillo <justin.tech96@gmail.com>
2024-01-12 01:20:50 +0800

    Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2024_01_11_082841) do
-=======
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_121326) do
->>>>>>> user_mailer
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
